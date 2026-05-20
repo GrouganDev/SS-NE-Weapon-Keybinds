14d2515592ed10e84d66f49b77a0fc2a1e083077
Kulgen <82848990+GrouganDev@users.noreply.github.com>
2026-05-19 22:35:08 -0400

    updated compile scripts so that all dependencies are hopefully installed

--- compile_python.bat
+++ compile_python.bat
@@ -1,2 +1,2 @@
 @echo off
-call "%~dp0venvWindows\Scripts\activate.bat" && pip install -r requirements.txt pyinstaller && pyinstaller --onefile "%~dp0\main.py" 
+py -m pip install --upgrade pip && py -m venv venvWindows && call "%~dp0venvWindows\Scripts\activate.bat" && pip install -r requirements.txt pyinstaller && pyinstaller --onefile "%~dp0\main.py" 
--- compile_python.sh
+++ compile_python.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo apt-get install python3-dev
+sudo apt install python3.12-venv
+python3 -m venv venvLinux
 source ./venvLinux/bin/activate
 pip install -r requirements.txt
 pyinstaller --onefile main.py
